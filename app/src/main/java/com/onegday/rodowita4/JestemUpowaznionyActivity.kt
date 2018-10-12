57a2ea069b85a5c646485214c36ea36f91d3a854
focus69a <40357086+focus69a@users.noreply.github.com>
2018-10-12 10:02:02 +0200

    Dziala JSON dla 2giej zakladki
    dziala json dla 2giej zakladki

--- app/src/main/java/com/onegday/rodowita4/JestemUpowaznionyActivity.kt
+++ app/src/main/java/com/onegday/rodowita4/JestemUpowaznionyActivity.kt
@@ -1,5 +1,6 @@
 package com.onegday.rodowita4
 
+import android.app.Activity
 import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
@@ -8,8 +9,11 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.ja_upowaznilem_item.view.*
 import kotlinx.android.synthetic.main.mnie_upowazniono_item.view.*
+import okhttp3.*
+import java.io.IOException
 
 
 class JestemUpowaznionyActivity : Fragment() {
@@ -24,19 +28,69 @@ class JestemUpowaznionyActivity : Fragment() {
         upowaznieniaRecyclerView = v.findViewById(R.id.recViewlistaUpowaznienID)
         upowaznieniaRecyclerView?.setHasFixedSize(true)   //to bylo usuniete a dodalem dziala tak samo
         upowaznieniaRecyclerView?.layoutManager = LinearLayoutManager(activity as Context?)   //moze getActivity() zamiast
-        upowaznieniaRecyclerView?.adapter = AdapterJestemUpowazniony(listaUpowaznien, context as Context?)  //moze getContext() zamiast
+//        upowaznieniaRecyclerView?.adapter = AdapterJestemUpowazniony(listaUpowaznien, context as Context?)  //moze getContext() zamiast
         return v
     }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        fetchJson()
     }
 
+    fun fetchJson() {
+        println("to jest test json")
+        val url = "https://flaskwj1.eu-gb.mybluemix.net/api/upowaznienia"
+
+        val request =Request.Builder().url(url).build()
+
+        val client = OkHttpClient()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onResponse(call: Call, response: Response) {
+                val body = response?.body()?.string()
+                println(body)
+
+                val gson = GsonBuilder().create()
+                val homeFeed = gson.fromJson(body, HomeFeed::class.java)
+
+
+                activity?.runOnUiThread{
+                    // !!!!!!!!!!!!!!!!!!! tutaj to trzeba jakos dodac
+                    upowaznieniaRecyclerView.adapter = AdapterJestemUpowazniony(homeFeed, context)
+
+                }
+            }
+            override fun onFailure(call: Call, e: IOException) {
+                println("Failure in execute callback WJ")
+            }
+        })
+    }
 }
+
+//================================klasa do trzymania jsona ==============================poczatek
+
+//{
+//    "dlaKogo": "Anna Jackiewicz",
+//    "firma": "PKOBP",
+//    "odKogo": "Waldemar Jackiewicz",
+//    "okres": "od 2018-09-01 do 2026-12-31",
+//    "zakres": "dyspozycje"
+//},
+
+class HomeFeed(val results: List<UpowaznieniaJSON>)
+
+class UpowaznieniaJSON(val dlaKogo: String, val firma: String, val odKogo: String, val okres : String, val zakres : String)
+
+
+
+//===============================klasa do trzymania jsona ==============================koniec
+
 //========================================================ADAPTER====================================
 
-class AdapterJestemUpowazniony(val upowaznienia: List<Jestem_Upowazniony_Dane>, val context: Context?) : RecyclerView.Adapter<AdapterJestemUpowazniony.myViewHolder>() {
+//class AdapterJestemUpowazniony(val upowaznienia: List<Jestem_Upowazniony_Dane>, val context: Context?) : RecyclerView.Adapter<AdapterJestemUpowazniony.myViewHolder>() {
+class AdapterJestemUpowazniony(val homeFeed: HomeFeed, val context: Context?) : RecyclerView.Adapter<AdapterJestemUpowazniony.myViewHolder>() {
+
     //var upowaznienie = items
     //var nazwyWlasne = items
     //var context = ctx
@@ -54,7 +108,8 @@ class AdapterJestemUpowazniony(val upowaznienia: List<Jestem_Upowazniony_Dane>, 
 
     //testowo
     override fun getItemCount(): Int {
-        return upowaznienia.size
+      //  return upowaznienia.size
+        return homeFeed.results.count()
 
     }
 
@@ -65,14 +120,21 @@ class AdapterJestemUpowazniony(val upowaznienia: List<Jestem_Upowazniony_Dane>, 
 
         //val upowaznienie = upowaznienia[position]
         //holder.nazwaWlasna?.text = upowaznienia[position].nazwa
-        holder.odKogo?.text = upowaznienia[position].odKogo
+//        holder.odKogo?.text = upowaznienia[position].odKogo
+//        //holder.dlaKogo?.text = upowaznienia[position].dlaKogo
+//        holder.okres?.text = upowaznienia[position].okres
+//        holder.firma?.text = upowaznienia[position].firma
+//        holder.zakres?.text = upowaznienia[position].zakres
+
+        holder.odKogo?.text = homeFeed.results[position].odKogo
         //holder.dlaKogo?.text = upowaznienia[position].dlaKogo
-        holder.okres?.text = upowaznienia[position].okres
-        holder.firma?.text = upowaznienia[position].firma
-        holder.zakres?.text = upowaznienia[position].zakres
+        holder.okres?.text = homeFeed.results[position].okres
+        holder.firma?.text = homeFeed.results[position].firma
+        holder.zakres?.text = homeFeed.results[position].zakres
 
 //        holder.itemView.textView_NazwaWlasnaID?.text = nazwyWlasne[position]
 //        holder.itemView.textViewOdID?.text = odKogo[position]
+
 
     }
 
@@ -99,3 +161,4 @@ class AdapterJestemUpowazniony(val upowaznienia: List<Jestem_Upowazniony_Dane>, 
         init { }
     }
 }
+
